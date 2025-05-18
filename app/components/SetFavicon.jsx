c57360ce6d508f08c0fb82df241c7dd68e36111d
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2025-05-18 22:19:42 +0700

    update: set fav icon

--- app/components/SetFavicon.jsx
+++ app/components/SetFavicon.jsx
@@ -4,10 +4,18 @@ import { useEffect } from "react";
 
 export default function SetFavicon({ iconUrl }) {
   useEffect(() => {
-    const favicon = document.querySelector("link[rel='icon']");
-    if (favicon && iconUrl) {
-      favicon.setAttribute("href", iconUrl);
+    if (!iconUrl) return;
+
+    let favicon = document.querySelector("link[rel='icon']");
+    
+    // Nếu chưa có thì tạo mới
+    if (!favicon) {
+      favicon = document.createElement("link");
+      favicon.setAttribute("rel", "icon");
+      document.head.appendChild(favicon);
     }
+
+    favicon.setAttribute("href", iconUrl);
   }, [iconUrl]);
 
   return null;
